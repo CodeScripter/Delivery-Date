90baabb1afa179e820c12ece1f03946693207c0f
Pradeep Beniwal <47877972+CodeScripter@users.noreply.github.com>
2020-11-07 17:47:02 +0530

    Add files via upload

--- date.py
+++ date.py
@@ -24,33 +24,25 @@ def add_day(current_date):
 #weekdays delivery date
 
 for i in range(len(week_days)): 
-	if(current_day==week_days[i])
+	if current_day == week_days[i]:
 		delivery_count = 1
-    		print(findDay(current_date)) 
-
-	elif(current_day==friday and current_time <= time )
-        	delivery_count = 3
-		print(findDay(current_date)) 
-
-		#increment_Function
-	elif(current_day==friday and current_time > time)
-		delivery_count = 4
-		print(findDay(current_date)) 
-
-        	        
+		print(add_day(current_date))
+	elif current_day == friday and current_time <= time:
+		delivery_count = 3
+		print(add_day(current_date)) 
+#increment_Function
+		elif current_day == friday and current_time > time:	
+		delivery_count = 4	
+		print(add_day(current_date)) 
 #weekends delivery date
-
-	if(current_day==week_ends[0])
+	if current_day == week_ends[0]:
 		delivery_count=3
-		print(findDay(current_date)) 
-
-		#increment_Function
-
-	elif(current_day==week_ends[1])
+		print(add_day(current_date))
+#increment_Function
+		elif current_day == week_ends[1]:
 		delivery_count=2
-		print(findDay(current_date)) 
-
-		#increment_Function
+		print(add_day(current_date)) 
+#increment_Function
 
 
 
