2ca6fb984528b16f36af2378734ae609084d3507
Pradeep Beniwal <47877972+CodeScripter@users.noreply.github.com>
2020-11-08 16:38:06 +0530

    Add files via upload

--- deliverydate.py
+++ deliverydate.py
@@ -4,39 +4,32 @@ from datetime import datetime
 import time
 import datetime
 import calendar 
-
-week_days = ["Monday","Tuesday","Wednesday","Thursday"]
-friday = "Friday"
-week_ends = ["Saturday","Sunday"]
+week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 current_day = datetime.date.today().strftime("%A")
 current_time = str(datetime.datetime.now().time())
 time = "15:00:00"
 current_date = date.today()
-#print(day)
-#print(time)
-#print(current_date)
 delivery_count = 0
-
-
 def add_day(current_date):
 	days = delivery_count
-	delevery_date = current_date + timedelta(delivery_count)
-	delivery_day = datetime.datetime.strptime(str(delevery_date), '%Y-%m-%d').weekday() 
+	delivery_date = current_date + timedelta(delivery_count)
+	print("delivery date and day:- ",delivery_date)
+	delivery_day = datetime.datetime.strptime(str(delivery_date), '%Y-%m-%d').weekday() 
 	return (calendar.day_name[delivery_day]) 
 
-if current_day =="Monday" or current_day =="Tuesday" or current_day =="Wednesday" or current_day =="Thursday":
+if current_day == week[0] or current_day == week[1] or current_day == week[2] or current_day == week[3]:
     delivery_count = 1
     print(add_day(current_date))
-elif current_day == friday and current_time <= time:
+elif current_day ==  week[4] and current_time <= time:
     delivery_count = 3
     print(add_day(current_date))
-elif current_day == friday and current_time > time:
+elif current_day == week[4] and current_time > time:
     delivery_count = 4
     print(add_day(current_date))
-elif current_day == week_ends[0]:
+elif current_day == week[5]:
     delivery_count=3
     print(add_day(current_date))
-elif current_day == week_ends[1]:
+elif current_day == week[6]:
     delivery_count=2
     print(add_day(current_date))
 else:
